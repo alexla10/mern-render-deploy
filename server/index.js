ee11a50088afeba6afccb76b60dd1218353d4df9
Alex Garnica <garnicaalex100@gmail.com>
2024-10-21 11:28:31 -0300

    primer despliegue 6

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PORT } from "./config.js";
+import { PORT,FRONT_END_URL } from "./config.js";
 import cors from 'cors'
 import router from './routes/tasks_routes.js';
 import {dirname,join} from 'path'
@@ -11,7 +11,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.json())
 app.use(cors({
     //si deseo que solo se conecte este
-    //origin:'http://localhost:5173'
+    origin:FRONT_END_URL
 }))
 
 app.use(express.static(join(__dirname,'../client/dist')))
