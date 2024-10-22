54b39cae0b373dcdd42cec15fa4080bea657dc9d
Alex Garnica <garnicaalex100@gmail.com>
2024-10-22 10:40:21 -0300

    actualizacion 2

--- client/src/api/tasks_api.js
+++ client/src/api/tasks_api.js
@@ -1,25 +1,27 @@
 import axios from 'axios'
 
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/tasks";
+
 export const createTaksRequest = async (task) => {
-    return await axios.post(`${import.meta.env.VITE_BACKEND_URL}`,task)
+    return await axios.post(`${URL}`,task)
 }
 
 export const getTasksRequest = async () => {
-    return await axios.get(`${import.meta.env.VITE_BACKEND_URL}`)
+    return await axios.get(`${URL}`)
 }
 
 export const deleteTasksRequest = async (id) => {
-    await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/${id}`)
+    await axios.delete(`${URL}/${id}`)
 }
 
 export const updateTasksRequest = async (id,newFields) => {
-    await axios.put(`${import.meta.env.VITE_BACKEND_URL}/${id}`,newFields)
+    await axios.put(`${URL}/${id}`,newFields)
 }
 
 export const getTaskRequest = async (id) => {
-    return await axios.get(`${import.meta.env.VITE_BACKEND_URL}/${id}`)
+    return await axios.get(`${URL}/${id}`)
 }
 
 export const toggleTaskDoneRequest = async (id,done) => {
-    await axios.put(`${import.meta.env.VITE_BACKEND_URL}/${id}`,{done,})
+    await axios.put(`${URL}/${id}`,{done,})
 }
