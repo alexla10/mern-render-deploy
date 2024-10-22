5b1c46fe77fc2f38302eb712dab56bd9ba2f8b6a
alexla10 <64673615+alexla10@users.noreply.github.com>
2024-10-22 15:51:04 -0300

    definitivo

--- client/src/api/tasks_api.js
+++ client/src/api/tasks_api.js
@@ -1,28 +1,13 @@
 import axios from 'axios'
 
-//const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/tasks";
-
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:4000", // URL base // Tiempo de espera en milisegundos
-    headers: {
-        'Content-Type': 'application/json',
-        // Puedes agregar otros encabezados aquí
-    },
-});
-
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/tasks";
 
 export const createTaksRequest = async (task) => {
     return await axios.post(`${URL}`,task)
 }
 
 export const getTasksRequest = async () => {
-   try {
-        const response = await axiosInstance.get('/tasks'); // Agrega la ruta relativa
-        return response.data;
-    } catch (error) {
-        console.error("Error al obtener las tareas:", error);
-        throw error; // Re-lanza el error
-    }
+   return await axios.get(`${URL}`)
 };
 
 export const deleteTasksRequest = async (id) => {
